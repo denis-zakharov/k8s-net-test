cd6df8d9aee149e8a173b6dff85683771b483119
Denis Zakharov <dizaharov@gmail.com>
2022-01-03 11:30:21 +0300

    kind dual net stack

--- kind/ingress.sh
+++ kind/ingress.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 # ingress
+#wget https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml -O ingress-deploy.yaml
+#for i in $(grep image: ingress-deploy.yaml | awk '{print $NF}' | sort -u); do docker pull $i; kind load docker-image $i; done
+#rm -fv ingress-deploy.yaml
+
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
 
 # wait ingress
